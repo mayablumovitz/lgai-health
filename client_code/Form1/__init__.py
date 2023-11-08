4d91d4e99f754d900e06342b60e831bf4287a094
maya.blumovitz@u.northwestern.edu <maya.blumovitz@u.northwestern.edu>
2023-11-08 18:41:27 +0000

    Created 1200px-Northwestern_Wildcats_logo.png; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,5 +5,9 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    alert('This is the app I just built')
     # Any code you write here will run before the form opens.
+
+  def Deidentify_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    pass
