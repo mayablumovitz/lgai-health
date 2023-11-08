db5ed3a3f5037c251f34fb33730516994ba5e142
maya.blumovitz@u.northwestern.edu <maya.blumovitz@u.northwestern.edu>
2023-11-08 20:51:29 +0000

    Edited ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -6,30 +6,27 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.reorde = "de"
-    self.txt = ""
-
-  def Deidentify_clicked(self, **event_args):
-    """This method is called when this radio button is selected"""
-    self.reorde = "de"
-
-  def Reidentify_clicked(self, **event_args):
-    """This method is called when this radio button is selected"""
-    self.reorde = "re"
 
   def Submit_click(self, **event_args):
     """This method is called when the button is clicked"""
-    # re-or-de = radioGroup1.
-    print(self.reorde)
-    # colab = anvil.server.call('LGAI_project')
+    if (self.Uploader.file is None):
+      alert("Upload your file to run.")
+      raise ValueError("No file was uploaded.")
+    if (self.Reidentify.selected):
+      result = anvil.server.call('reidentify', self.Uploader.file)
+    else:
+      result = anvil.server.call('deidentify', self.Uploader.file)
+    self.Output.text = result
+    print(result)
 
-  def file_loader_1_change(self, file, **event_args):
+  def Uploader_change(self, file, **event_args):
     """This method is called when a new file is loaded into this FileLoader"""
-    try:
-        if file.name.endswith('.txt'):
-            file_content = file.read().decode('utf-8')
-            self.txt = file_content
-        else:
-            raise ValueError("Invalid file format. Please load a .txt file.")
-    except Exception as e:
-        return f"Error: {str(e)}"
+    print("uploaded")
+
+  def Reidentify_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    pass
+
+  def Deidentify_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    pass
