1476f6e8cb0d2630ae0f10f9dbced70832ed0ea0
maya.blumovitz@u.northwestern.edu <maya.blumovitz@u.northwestern.edu>
2023-11-15 21:11:53 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -12,13 +12,15 @@ class Form1(Form1Template):
     if (self.Uploader.file is None):
       alert("Upload your file to run.")
       raise ValueError("No file was uploaded.")
+    content = self.Uploader.file.get_bytes()
+    print(str(content))
     if (self.Reidentify.selected):
       print("running reidentify")
-      result = anvil.server.call('reidentify', self.Uploader.file)
+      result = anvil.server.call('reidentify', str(content))
     else:
       print("running deidentify")
-      result = anvil.server.call('deidentify', self.Uploader.file)
-    self.Output.text = result
+      result = anvil.server.call('deidentify', str(content))
+    self.Output.text = 'FINAL REPORT\n' + result['generated_text'].split('FINAL REPORT', 2)[2].strip()
     print(result)
 
   def Uploader_change(self, file, **event_args):
